8d08b03418d3f100d7f3009ec4ca4f58e3ee9d28
logroid <yoheiowa+github@gmail.com>
2018-05-27 10:36:55 +0900

    更新

--- script/tablesorter.js
+++ script/tablesorter.js
@@ -1,6 +1,13 @@
 $(function() {
-  $('table.tablesorter').tablesorter({
+  $('table.tablesorter:not([data-tablesorter-widgets])').tablesorter({
     theme: "materialize",
     widgets: ["zebra"],
+  });
+  $('table.tablesorter[data-tablesorter-widgets]').each(function(i, table) {
+    var $table = $(table),
+      widget = $table.data('tablesorter-widgets');
+    $table.tablesorter({
+      widgets: widget
+    });
   });
 });
