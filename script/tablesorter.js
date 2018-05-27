e4c7934dd11deaf9550cd3f043d55ca30b2a5512
logroid <yoheiowa+github@gmail.com>
2018-05-27 13:32:23 +0900

    更新

--- script/tablesorter.js
+++ script/tablesorter.js
@@ -1,16 +1,33 @@
 $(function() {
-  $('table.tablesorter:not([data-tablesorter-widgets])').tablesorter({
+$('table.tablesorter:not([data-tablesorter-widgets])').tablesorter({
+  theme: 'materialize',
+  widthFixed: true,
+  widgets: ["zebra"]
+});
+$('table.tablesorter[data-tablesorter-widgets]').each(function(i, table) {
+  var $table = $(table),
+    widget = $table.data('tablesorter-widgets'),
+    widgetOption = {};
+  if (widget.indexOf('columnSelector') > -1) {
+    var $fieldSet = $('<fieldset>'),
+      $legend = $('<legend>').text('表示カラム選択'),
+      $columnSelector = $('<div class="column-selector">');
+    $fieldSet.append($legend).append($columnSelector);
+    $table.before($fieldSet)
+    widgetOption = $.extend(widgetOption, {
+      columnSelector_container: $columnSelector,
+      columnSelector_classHasSpan: 'has-span',
+      columnSelector_layout: '<label><input type="checkbox">{name}</label>',
+      columnSelector_mediaquery: false,
+    });
+  }
+
+  $table.tablesorter({
     theme: 'materialize',
     widthFixed: true,
-    widgets: ["zebra"]
+    widgets: widget,
+    widgetOptions: widgetOption
   });
-  $('table.tablesorter[data-tablesorter-widgets]').each(function(i, table) {
-    var $table = $(table),
-      widget = $table.data('tablesorter-widgets');
-    $table.tablesorter({
-      theme: 'materialize',
-      widthFixed: true,
-      widgets: widget
-    });
-  });
+});
+});
 });
