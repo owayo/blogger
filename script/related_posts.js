2dcc96b341c6b678f487997bea6d5fa1f15cd1af
logroid <yoheiowa+github@gmail.com>
2018-05-28 21:01:37 +0900

    更新

--- script/related_posts.js
+++ script/related_posts.js
@@ -14,10 +14,10 @@ Logroid.related_posts = Logroid.related_posts || (function(logroid) {
   function getFeed(label) {
     console.info(label)
     $.ajax({
-      url: 'https://logroid.blogspot.com/feeds/posts/default/-/' + label,
+      url: "https://query.yahooapis.com/v1/public/yql?q=select%20updated%2Ctitle%2Clink%20from%20rss%20where%20url%20%3D%20'https%3A%2F%2Flogroid.blogspot.com%2Ffeeds%2Fposts%2Fdefault%2F-%2F" + label + "%3Falt%3Drss'&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys",
       type: 'GET',
       cache: false,
-      dataType: 'xml',
+      dataType: 'json',
       timeout: 5000,
       success: function(res, status) {
         console.dir(res);
