841b963ce91c45a491c93381f3d81d0db84ee455
logroid <yoheiowa+github@gmail.com>
2018-05-28 22:06:38 +0900

    更新

--- script/related_posts.js
+++ script/related_posts.js
@@ -1,7 +1,8 @@
 var Logroid = Logroid || {};
 
 Logroid.related_posts = Logroid.related_posts || (function(logroid) {
-  var feed = [];
+  var KEY_PREFIX = 'related_posts-';
+  var feed = {};
 
   function getLabel($entry) {
     var $labels = $entry.find('.post-footer .post-labels a[rel=tag]');
@@ -13,8 +14,11 @@ Logroid.related_posts = Logroid.related_posts || (function(logroid) {
     return null;
   }
 
-  function addFeed(json) {
-    feed.push({
+  function addFeed(label, json) {
+    if (feed[label] == null) {
+      feed[label] = []
+    }
+    feed[label].push({
       link: json.link,
       title: json.title,
       updated: Date.parse(json.updated)
@@ -23,6 +27,7 @@ Logroid.related_posts = Logroid.related_posts || (function(logroid) {
 
   function drawFeed() {
     console.dir(feed)
+
   }
 
   function getFeed(labels, index) {
@@ -30,17 +35,23 @@ Logroid.related_posts = Logroid.related_posts || (function(logroid) {
       drawFeed();
       return
     }
-    // https://developer.yahoo.com/yql/
     var label = labels[index];
-    $.getJSON("https://query.yahooapis.com/v1/public/yql?q=select%20updated%2Ctitle%2Clink%20from%20rss%20where%20url%20%3D%20'https%3A%2F%2Flogroid.blogspot.com%2Ffeeds%2Fposts%2Fdefault%2F-%2F" + label + "%3Falt%3Drss'&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys", function(json) {
-      console.dir(json);
-      if (json != null && json.query != null && json.query.results != null && json.query.results.item != null) {
-        $.each(json.query.results.item, function(i, item) {
-          addFeed(item);
-        });
-      }
+    if (StorageKeyExist(KEY_PREFIX + label)) {
+      addFeed(label, StorageLoad(KEY_PREFIX + label));
       getFeed(labels, index + 1);
-    });
+    } else {
+      // https://developer.yahoo.com/yql/
+      $.getJSON("https://query.yahooapis.com/v1/public/yql?q=select%20updated%2Ctitle%2Clink%20from%20rss(0%2C5)%20where%20url%20%3D%20'https%3A%2F%2Flogroid.blogspot.com%2Ffeeds%2Fposts%2Fdefault%2F-%2F" + label + "%3Falt%3Drss'&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys", function(json) {
+        console.dir(json);
+        if (json != null && json.query != null && json.query.results != null && json.query.results.item != null) {
+          $.each(json.query.results.item, function(i, item) {
+            StorageSave(KEY_PREFIX + label, item);
+            addFeed(label, item);
+          });
+        }
+        getFeed(labels, index + 1);
+      });
+    }
   }
   return {
     start: function() {
