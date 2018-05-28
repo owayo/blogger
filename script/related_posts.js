6a186848bf73d2c907d806879d78fbd6a5703860
logroid <yoheiowa+github@gmail.com>
2018-05-28 21:39:55 +0900

    更新

--- script/related_posts.js
+++ script/related_posts.js
@@ -1,6 +1,8 @@
 var Logroid = Logroid || {};
 
 Logroid.related_posts = Logroid.related_posts || (function(logroid) {
+  var feed = [];
+
   function getLabel($entry) {
     var $labels = $entry.find('.post-footer .post-labels a[rel=tag]');
     if ($labels.length > 0) {
@@ -11,36 +13,47 @@ Logroid.related_posts = Logroid.related_posts || (function(logroid) {
     return null;
   }
 
-  function getFeed(label) {
-    // https://developer.yahoo.com/yql/
-    $.getJSON("https://query.yahooapis.com/v1/public/yql?q=select%20updated%2Ctitle%2Clink%20from%20rss%20where%20url%20%3D%20'https%3A%2F%2Flogroid.blogspot.com%2Ffeeds%2Fposts%2Fdefault%2F-%2F" + label + "%3Falt%3Drss'&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys", function(res) {
-      console.dir(res);
+  function addFeed(json) {
+    feed.push({
+      link: json.link,
+      title: json.title,
+      updated: Date.parse(json.updated)
     });
-    $.ajax({
-      url: "https://query.yahooapis.com/v1/public/yql?q=select%20updated%2Ctitle%2Clink%20from%20rss%20where%20url%20%3D%20'https%3A%2F%2Flogroid.blogspot.com%2Ffeeds%2Fposts%2Fdefault%2F-%2F" + label + "%3Falt%3Drss'&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys",
-      type: 'GET',
-      cache: false,
-      dataType: 'json',
-      timeout: 5000,
-      success: function(res, status) {
-        console.dir(res);
-      },
-      error: function(req, status, errorThrown) {
-        console.dir(req);
-        console.dir(status);
-        console.dir(errorThrown);
+  }
+
+  function drawFeed() {
+    console.dir(feed)
+  }
+
+  function getFeed(labels, index) {
+    if (labels.length <= index) {
+      drawFeed();
+    }
+    // https://developer.yahoo.com/yql/
+    var label = labels[index];
+    $.getJSON("https://query.yahooapis.com/v1/public/yql?q=select%20updated%2Ctitle%2Clink%20from%20rss%20where%20url%20%3D%20'https%3A%2F%2Flogroid.blogspot.com%2Ffeeds%2Fposts%2Fdefault%2F-%2F" + label + "%3Falt%3Drss'&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys", function(json) {
+      console.dir(json);
+      $.each(json.query.results.item, function(i, item) {
+        addFeed(item);
+      });
+      if (labels.length > index + 1) {
+        getFeed(labels, index + 1)
       }
     });
   }
   return {
     start: function() {
+      var labels = []
       $('.post.hentry').each(function(i, entry) {
-        var $entry = $(entry),
-          labels = getLabel($entry);
-        $.each(labels, function(ii, label) {
-          getFeed(label);
-        });
+        var $entry = $(entry);
+        labels.push(getLabel($entry));
       });
+      Array.prototype.concat.apply([], labels);
+      labels = labels.filter(function(x, i, self) {
+        return self.indexOf(x) === i;
+      });
+      console.dir(labels);
+      getFeed(labels, 0);
     }
 
 
