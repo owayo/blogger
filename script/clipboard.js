1236370858b57bb3b7167cf0fbb61a9e746f1604
logroid <yoheiowa+github@gmail.com>
2020-05-03 19:25:37 +0900

    Add files via upload

--- script/clipboard.js
+++ script/clipboard.js
@@ -3,13 +3,13 @@ Vue.use(Buefy, {
   defaultIconPack: 'fas',
 });
 Vue.config.productionTip = false;
-const clipboardVue = new Vue(
+const clipboardVue = new Vue({
   methods: {
     toast(opt) {
       this.$buefy.toast.open(opt)
     }
   }
-);
+});
 clipboard.on('success', e => {
   clipboardVue.toast({
     message: 'コピーしました',
